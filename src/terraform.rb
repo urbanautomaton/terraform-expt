222f16c6f57ef1633274221c1700ea4b4038ba6e
Simon Coffey <simon@urbanautomaton.com>
2015-10-28 19:11:26 +0000

    Add security groups

--- src/terraform.rb
+++ src/terraform.rb
@@ -4,6 +4,34 @@ REGION      = "eu-west-1"
 NAT_AMI     = "ami-14913f63" # amzn-ami-vpc-nat-hvm-2014.09.1.x86_64-gp2
 INTERNET_GW = "#{ENVIRONMENT}-gw"
 
+module SG
+  class << self
+    def allow_http(source)
+      {
+        from_port: 80,
+        to_port: 80,
+        protocol: "tcp"
+      }.merge(source)
+    end
+
+    def allow_https(source)
+      {
+        from_port: 443,
+        to_port: 443,
+        protocol: "tcp"
+      }.merge(source)
+    end
+
+    def allow_ssh(source)
+      {
+        from_port: 22,
+        to_port: 22,
+        protocol: "tcp"
+      }.merge(source)
+    end
+  end
+end
+
 CIDR_BLOCKS = {
   public: {
     a: '10.0.0.0/24',
@@ -26,22 +54,22 @@ provider :aws do
   region REGION
 end
 
-resource :aws_vpc, ENVIRONMENT do
+aws_vpc ENVIRONMENT do
   cidr_block "10.0.0.0/16"
   enable_dns_support true
   enable_dns_hostnames true
 end
 
-resource :aws_internet_gateway, INTERNET_GW do
+aws_internet_gateway INTERNET_GW do
   vpc_id id_of("aws_vpc", ENVIRONMENT)
 end
 
-resource :aws_vpc_dhcp_options, ENVIRONMENT do
+aws_vpc_dhcp_options ENVIRONMENT do
   domain_name "#{ENVIRONMENT}.urbanautomaton.com"
   domain_name_servers ["AmazonProvidedDNS"]
 end
 
-resource :aws_vpc_dhcp_options_association, ENVIRONMENT do
+aws_vpc_dhcp_options_association ENVIRONMENT do
   vpc_id id_of("aws_vpc", ENVIRONMENT)
   dhcp_options_id id_of("aws_vpc_dhcp_options", ENVIRONMENT)
 end
@@ -52,13 +80,13 @@ AZS.each do |az|
   nat_name        = "#{ENVIRONMENT}-nat-01#{az}"
 
   # Public subnets
-  resource :aws_subnet, pub_subnet_name do
+  aws_subnet pub_subnet_name do
     vpc_id id_of("aws_vpc", ENVIRONMENT)
     availability_zone "#{REGION}#{az}"
     cidr_block CIDR_BLOCKS[:public][az]
   end
 
-  resource :aws_route_table, pub_subnet_name do
+  aws_route_table pub_subnet_name do
     vpc_id id_of("aws_vpc", ENVIRONMENT)
     route({
       cidr_block: "0.0.0.0/0",
@@ -66,12 +94,12 @@ AZS.each do |az|
     })
   end
 
-  resource :aws_route_table_association, pub_subnet_name do
+  aws_route_table_association pub_subnet_name do
     route_table_id id_of("aws_route_table", pub_subnet_name)
     subnet_id id_of("aws_subnet", pub_subnet_name)
   end
 
-  resource :aws_instance, nat_name do
+  aws_instance nat_name do
     ami NAT_AMI
     availability_zone "#{REGION}#{az}"
     subnet_id id_of("aws_subnet", pub_subnet_name)
@@ -81,13 +109,13 @@ AZS.each do |az|
   end
 
   # Private subnets
-  resource :aws_subnet, int_subnet_name do
+  aws_subnet int_subnet_name do
     vpc_id id_of("aws_vpc", ENVIRONMENT)
     availability_zone "#{REGION}#{az}"
     cidr_block CIDR_BLOCKS[:private][az]
   end
 
-  resource :aws_route_table, int_subnet_name do
+  aws_route_table int_subnet_name do
     vpc_id id_of("aws_vpc", ENVIRONMENT)
     route({
       cidr_block: "0.0.0.0/0",
@@ -95,8 +123,52 @@ AZS.each do |az|
     })
   end
 
-  resource :aws_route_table_association, int_subnet_name do
+  aws_route_table_association int_subnet_name do
     route_table_id id_of("aws_route_table", int_subnet_name)
     subnet_id id_of("aws_subnet", int_subnet_name)
   end
+end
+
+#
+# Security Groups
+#
+
+aws_security_group "#{ENVIRONMENT}-bastion" do
+  vpc_id id_of("aws_vpc", ENVIRONMENT)
+  description "Bastion hosts in the #{ENVIRONMENT} environment"
+
+  ingress(
+    [
+      SG.allow_ssh(cidr_blocks: ["0.0.0.0/0"])
+    ]
+  )
+end
+
+aws_security_group "#{ENVIRONMENT}-default" do
+  vpc_id id_of("aws_vpc", ENVIRONMENT)
+  description "All hosts in the #{ENVIRONMENT} environment"
+
+  ingress(
+    [
+      SG.allow_ssh(
+        security_groups: [id_of("aws_security_group", "#{ENVIRONMENT}-bastion")]
+      )
+    ]
+  )
+end
+
+aws_security_group "#{ENVIRONMENT}-nat" do
+  vpc_id id_of("aws_vpc", ENVIRONMENT)
+  description "NAT instances in #{ENVIRONMENT} environment"
+
+  ingress(
+    [
+      SG.allow_http(
+        security_groups: [id_of("aws_security_group", "#{ENVIRONMENT}-default")]
+      ),
+      SG.allow_https(
+        security_groups: [id_of("aws_security_group", "#{ENVIRONMENT}-default")]
+      )
+    ]
+  )
 end
